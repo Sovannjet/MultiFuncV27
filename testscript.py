0627f7b336c818180e93f21e278d99cae2525bce
Sovannjet <85751729+Sovannjet@users.noreply.github.com>
2023-03-12 00:31:24 -0800

    Delete testscript.py

--- testscript.py
+++
@@ -1,7 +0,0 @@
-async def main(message, client):
-  await message.channel.send('type something')
-  msg = await client.wait_for('message')
-  await message.channel.send('{.content}!'.format(msg))
-
-if __name__ == "__main__":
-  main()
